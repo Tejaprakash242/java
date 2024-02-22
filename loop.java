2cf304ff27241c498e7c6117d9c6a56184db594a
Tejaprakash Reddy <143252491+Tejaprakash242@users.noreply.github.com>
2024-02-22 22:24:55 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,11 @@
+public class loop {
+    public static void main(String a[])
+    {
+        int i =1;
+        while()
+        {
+            System.out.println("Teja");
+        }
+    }
+    
+}
